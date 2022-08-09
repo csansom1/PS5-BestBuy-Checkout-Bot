b42d715a2bb8d428f529e3d7f0fd15c361bfbbc1
csansom1 <97200802+csansom1@users.noreply.github.com>
2022-08-09 14:04:02 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import ElementNotInteractableException, NoSuchElementException, StaleElementReferenceException
 from random import randint, randrange
 import time
-import  info
+import info
 import random
 
 DRIVER_PATH = "C:\Program Files (x86)\chromedriver.exe"
-AMAZON_URL = "https://www.amazon.ca/Playstation-3006648-PlayStation-Disc-Edition/dp/B09DPJ2TGW/ref=sr_1_3?brr=1&qid=1641519145&rd=1&s=videogames&sr=1-3"
-AMAZON_TEST_URL = "https://www.amazon.ca/Toothpicks-Reusable-Container-Cocktail-Appetizers/dp/B08Y5WPCKM/ref=sr_1_6?crid=1R9OCVAUGCC28&keywords=toothpick&qid=1641616645&sprefix=toothpick%2Caps%2C73&sr=8-6" ## "https://www.amazon.ca/Namco-Bandai-13011-Tales-Arise/dp/B093B218BN/ref=sr_1_1?crid=3L65Q9ACAZOID&keywords=tales+of+arise+ps5&qid=1641519651&sprefix=tales%2Caps%2C96&sr=8-1"
-WAIT_TIME = 7
-PRICE_LIMIT = 700.00
+BEST_BUY_URL = "https://www.bestbuy.ca/en-ca/product/playstation-5-console/15689336"
+BEST_BUY_TEST_URL = "https://www.bestbuy.ca/en-ca/product/the-legend-of-zelda-link-s-awakening-switch/12601646"
+WAIT_TIME = 5
 
 
-class PS5AmazonBot:
-	def __init__(self, username, password):
+class PS5BestBuyBot:
+	def __init__(self, username, password, cvv):
 		self.username = username
 		self.password = password
-		self.driver = webdriver.Chrome(DRIVER_PATH)
+		self.cvv = cvv
+		options = Options()
+		options.add_argument('--disable-blink-features=AutomationControlled')
+		self.driver = webdriver.Chrome(DRIVER_PATH, options=options)
 
 	## Sign into site with the product
 	def signIn(self):
@@ -26,20 +29,17 @@ class PS5AmazonBot:
 		driver = self.driver  ## Navigate to URL
 
 		## Enter Username
-		username_elem = driver.find_element_by_xpath("//input[@name='email']")
+		username_elem = driver.find_element_by_xpath("//input[@name='username']")
 		username_elem.clear()
 		username_elem.send_keys(self.username)
-
-		time.sleep(randint(WAIT_TIME >> 1, WAIT_TIME))
-		username_elem.send_keys(Keys.RETURN)
 		time.sleep(randint(WAIT_TIME >> 1, WAIT_TIME))
 
 		## Enter Password
 		password_elem = driver.find_element_by_xpath("//input[@name='password']")
 		password_elem.clear()
 		password_elem.send_keys(self.password)
-
 		time.sleep(randint(WAIT_TIME >> 1, WAIT_TIME))
+
 		password_elem.send_keys(Keys.RETURN)
 		time.sleep(randint(WAIT_TIME >> 1, WAIT_TIME))
 
@@ -47,32 +47,63 @@ class PS5AmazonBot:
 	def findProduct(self):
 		""" Finds the product with global link. """
 		driver = self.driver
-		#driver.get(AMAZON_TEST_URL)
-		driver.get(AMAZON_URL)
+		#driver.get(BEST_BUY_TEST_URL)
+		driver.get(BEST_BUY_URL)
 		time.sleep(randint(WAIT_TIME >> 1, WAIT_TIME))
 
 		## If the product is not available, wait until it is available
-		isAvailable = self.isProductAvailable()
+		is_available = self.isProductAvailable()
 
-		while isAvailable == 'Currently unavailable.' or isAvailable > PRICE_LIMIT:
-			#driver.get(AMAZON_TEST_URL)
-			driver.get(AMAZON_URL)
+		while is_available == 'Coming soon':
+			#driver.get(BEST_BUY_TEST_URL)
+			driver.get(BEST_BUY_URL)
 			time.sleep(randint(WAIT_TIME >> 1, WAIT_TIME))
-			isAvailable = self.isProductAvailable()
+			is_available = self.isProductAvailable()
 
-		## Buy Now
-		buy_now = driver.find_element_by_name('submit.buy-now')
-		buy_now.click()
+		## Restart browser if access denied
+		if is_available == "Access denied":
+			self.closeBrowser()
+			time.sleep(randint(WAIT_TIME >> 1, WAIT_TIME))
+			options = Options()
+			options.add_argument('--disable-blink-features=AutomationControlled')
+			self.driver = webdriver.Chrome(DRIVER_PATH, options=options)
+			self.findProduct()
+			return
+
+		## Add to cart
+		add_to_cart = driver.find_element_by_class_name('addToCartButton')
+		add_to_cart.click()
 		time.sleep(randint(WAIT_TIME >> 1, WAIT_TIME))
+
+		## View cart
+		view_cart = driver.find_element_by_class_name('viewCart')
+		view_cart.click()
+		time.sleep(randint(WAIT_TIME >> 1, WAIT_TIME))
+
+		## Continue to checkout
+		continue_to_checkout = driver.find_element_by_class_name('continueToCheckout_3Dgpe')
+		continue_to_checkout.click()
+		time.sleep(randint(WAIT_TIME >> 1, WAIT_TIME))
+
 		self.signIn()
+		time.sleep(8)
+
+		## Confirm CVV
+		cvv_field = driver.find_element_by_xpath("//input[@name='cvv']")
+		cvv_field.clear()
+		cvv_field.send_keys(self.cvv)
 		time.sleep(randint(WAIT_TIME >> 1, WAIT_TIME))
 
 		## Place Order
-		place_order = driver.find_element_by_name('placeYourOrder1')
-		#place_order_text = place_order.get_attribute('value')
+		place_order = driver.find_element_by_class_name('order-now')
+		#place_order_type = place_order.get_attribute('type')
 		time.sleep(randint(WAIT_TIME >> 1, WAIT_TIME))
-		#print(f'***** PLACE ORDER: {place_order_text}')
+		#print(f'***** PLACE ORDER: {place_order_type}')
 		place_order.click()
+		time.sleep(randint(WAIT_TIME >> 1, WAIT_TIME))
+
+		button = driver.driver.find_element_by_xpath("//input[@type='button']")
+		button.send_keys(Keys.RETURN)
 		time.sleep(randint(WAIT_TIME >> 1, WAIT_TIME))
 		print(f'***** ORDER PLACED')
 
@@ -80,12 +111,12 @@ class PS5AmazonBot:
 		""" Checks if product is available. """
 		driver = self.driver
 		try:
-			price = driver.find_element_by_class_name('_p13n-desktop-sims-fbt_price_p13n-sc-price__bCZQt').text
-			print(f'***** PRICE: {price}')
-			return float(price[1:])
+			availability = driver.find_element_by_class_name('availabilityMessage_ig-s5').text
+			print(f'***** AVAILABILITY: {availability}')
+			return availability
 		except NoSuchElementException:
-			print(f'***** CURRENTLY UNAVAILABLE.')
-			return 'Currently unavailable.'
+			print('***** ACCESS DENIED')
+			return "Access denied"
 
 	def closeBrowser(self):
 		""" Closes browser """
@@ -94,6 +125,6 @@ class PS5AmazonBot:
 
 
 if __name__ == '__main__':
-	shopBot = PS5AmazonBot(username=info.username, password=info.password)
+	shopBot = PS5BestBuyBot(username=info.username, password=info.password, cvv=info.cvv)
 	shopBot.findProduct()
 	shopBot.closeBrowser()
